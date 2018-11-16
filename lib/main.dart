39e166e62afd937a9a39443dc683cc8460495e7a
Rian Saputra <34260151+rians4042@users.noreply.github.com>
2018-11-16 16:41:23 +0700

    drawler

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,41 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(new MaterialApp(
   title: "Pabrik_Plastik",
-  home: new HalamanSatu(),
+  home: new Home(),
 ));
 }
 
-class HalamanSatu extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override 
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
-  Widget  build(BuildContext context) {
+  Widget build(BuildContext context) {
     return new Scaffold(
-    appBar: new AppBar(
-      title: new Text("Pabrik Plastik"),
-    ),
+      appBar: new AppBar(title: new Text("Pabrik Plastik"),),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new ListTile( 
+              title: new Text("Retail"),
+              trailing: new Icon(Icons.content_copy),
+            ),
+            new ListTile(
+              title: new Text("Suplier"),
+              trailing: new Icon(Icons.donut_small),
+              contentPadding: EdgeInsets.only(left: 50.0, right: 15.0),
+            ),
+
+            new ListTile(
+              title: new Text("Produksi"),
+              trailing: new Icon(Icons.toys),
+            )
+          ],
+        ),
+      ),
+      body: new Container(),
     );
   }
 }
-
