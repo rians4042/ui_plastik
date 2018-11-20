58572503f796dd8ca1c15185a3c8d133c54fd50b
rians4042 <rians4042@gmail.com>
2018-11-20 13:03:28 +0700

    ongoing homescreen

---
+++ lib/example.dart
@@ -0,0 +1,23 @@
+//import 'package:flutter/material.dart';
+
+//class Example extends StatelessWidget {
+//final Widget title;
+//final Widget description;
+
+// constructor
+// Example({?@required this.title, @required this.description});
+
+//@override
+// Widget build(BuildContext context) {
+//  return Container(
+//   child: Column(
+//    children: <Widget>[
+//     title,
+//    description
+// Text(title),
+// Text(description),
+//  ],
+//  ),
+// );
+// }
+//}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(new MaterialApp(
-  title: "Pabrik_Plastik",
-  home: new Home(),
-));
+    title: "Pabrik_Plastik",
+    home: new Home(),
+  ));
 }
 
 class Home extends StatefulWidget {
-  @override 
+  @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  final List<String> names = ["äzhar", "rian"];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Pabrik Plastik"),),
+      appBar: new AppBar(
+        title: new Text("Pabrik Plastik"),
+      ),
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
-            new ListTile( 
+            new ListTile(
               title: new Text("Retail"),
               trailing: new Icon(Icons.content_copy),
             ),
@@ -29,7 +33,6 @@ class _HomeState extends State<Home> {
               trailing: new Icon(Icons.donut_small),
               contentPadding: EdgeInsets.only(left: 50.0, right: 15.0),
             ),
-
             new ListTile(
               title: new Text("Produksi"),
               trailing: new Icon(Icons.toys),
@@ -37,7 +40,45 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
-      body: new Container(),
+      body: Container(
+        color: Colors.blue,
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.30,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: names.length,
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    width: 80.0,
+                    margin: EdgeInsets.symmetric(horizontal: 8.0),
+                    color: Colors.red,
+                    child: Text(names[index]),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+                child: GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this would produce 2 rows.
+              crossAxisCount: 2,
+              // Generate 100 Widgets that display their index in the List
+              children: List.generate(100, (index) {
+                return Center(
+                  child: Text(
+                    'Item $index',
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                );
+              }),
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
