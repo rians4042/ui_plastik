ddd2f4b0c2c90cc9bf825f7b661fe54efdb23509
rians4042 <rians4042@gmail.com>
2018-11-25 12:50:09 +0700

    grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plastik_ui/screens/item-product/model/item-product.dart';
 
 void main() {
   runApp(new MaterialApp(
@@ -14,16 +15,54 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final List<String> names = ["äzhar", "rian"];
+  // 2 data map / obj
+
+  // final Map<String, dynamic> examples = {
+  //   "name": "azhar prabudi",
+  // };
+
+  // examples["name"]
+
+  // obj
+  final List<Example> examples = [
+    Example(
+        icon: Icon(
+          Icons.kitchen,
+          size: 40.0,
+        ),
+        label: Text("Katergori")),
+    Example(
+        icon: Icon(
+          Icons.receipt,
+          size: 40.0,
+        ),
+        label: Text("Item")),
+    Example(
+        icon: Icon(
+          Icons.people,
+          size: 40.0,
+        ),
+        label: Text("Seller")),
+    Example(icon: Icon(Icons.import_export, size: 40.0), label: Text("")),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Pabrik Plastik"),
+        title: new Text("plastik"),
       ),
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountEmail: Text("hallo@gmail.com"),
+              accountName: Text("suparman"),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://i.ytimg.com/vi/qE9lgnBaIUg/maxresdefault.jpg"),
+              ),
+            ),
             new ListTile(
               title: new Text("Retail"),
               trailing: new Icon(Icons.content_copy),
@@ -31,7 +70,7 @@ class _HomeState extends State<Home> {
             new ListTile(
               title: new Text("Suplier"),
               trailing: new Icon(Icons.donut_small),
-              contentPadding: EdgeInsets.only(left: 50.0, right: 15.0),
+              // contentPadding: EdgeInsets.only(left: 50.0, right: 15.0),
             ),
             new ListTile(
               title: new Text("Produksi"),
@@ -41,7 +80,7 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: Container(
-        color: Colors.blue,
+        //color: Colors.blue,
         child: Column(
           children: <Widget>[
             Container(
@@ -53,29 +92,60 @@ class _HomeState extends State<Home> {
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
-                    width: 80.0,
+                    width: 370.0,
                     margin: EdgeInsets.symmetric(horizontal: 8.0),
-                    color: Colors.red,
+                    color: Colors.grey[300],
                     child: Text(names[index]),
                   );
                 },
               ),
             ),
             Expanded(
-                child: GridView.count(
-              // Create a grid with 2 columns. If you change the scrollDirection to
-              // horizontal, this would produce 2 rows.
-              crossAxisCount: 2,
-              // Generate 100 Widgets that display their index in the List
-              children: List.generate(100, (index) {
-                return Center(
-                  child: Text(
-                    'Item $index',
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                );
-              }),
-            )),
+              child: GridView.builder(
+                // Create a grid with 2 columns. If you change the scrollDirection to
+                // horizontal, this would produce 2 rows.
+                // crossAxisCount: 2,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  //crossAxisSpacing: 5.0,
+                  //mainAxisSpacing: 0.0,
+                ),
+                // Generate 100 Widgets that display their index in the List
+                itemCount: examples.length,
+                itemBuilder: (BuildContext ctx, int index) {
+                  return GestureDetector(
+                      onTap: () {
+                        print("kuda $index");
+                      },
+                      child: new Container(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            new BoxShadow(
+                              color: Colors.grey[100],
+                              blurRadius: 7.0,
+                            ),
+                          ],
+                          //border: Border.all(color: Colors.black38),
+                        ),
+                        margin: const EdgeInsets.all(25.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            examples[index].icon,
+                            examples[index].label,
+                          ],
+                        ),
+                      ));
+                },
+                // itemBuilder: List.generate(4, (index) {
+                //   return Center(
+                //       child: GestureDetector(
+                //           onTap: () {},
+                //           child: Container(child: Icon(Icons.home))));
+                // }),
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/screens/item-product/model/item-product.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Example {
+  Widget icon;
+  Widget label;
+
+  Example({
+    @required this.icon,
+    @required this.label,
+  });
+}
