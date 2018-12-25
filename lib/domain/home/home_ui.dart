4837324a19b740a79432483e65af4e68da2cffc8
rians4042 <rians4042@gmail.com>
2018-12-25 12:37:09 +0700

    bottom tabbar failed

--- lib/domain/home/home_ui.dart
+++ lib/domain/home/home_ui.dart
@@ -41,12 +41,41 @@ final List<Example> examples = [
   ),
 ];
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    controller = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Plastik"),
+      ),
+      bottomNavigationBar: new Material(
+        color: Colors.amber,
+        child: new TabBar(
+          controller: controller,
+          tabs: <Widget>[
+            new Tab(
+              icon: new Icon(Icons.home),
+              text: 'Home',
+            ),
+            new Tab(
+              icon: new Icon(Icons.monetization_on),
+              text: 'Transaction',
+            ),
+          ],
+        ),
       ),
       body: Center(
         child: Card(
