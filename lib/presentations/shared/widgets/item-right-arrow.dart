529c3f687c2f207f78b59195c398b5fb0d2fa074
azharprabudi <azharprabui@gmail.com>
2018-12-30 16:16:58 +0700

    add item right arrow

---
+++ lib/presentations/shared/widgets/item-right-arrow.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:plastik_ui/values/colors.dart';
+
+class ItemRightArrow extends StatelessWidget {
+  final String label;
+  final Function onPress;
+
+  ItemRightArrow({
+    @required this.label,
+    this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              width: 1,
+              color: GREY_COLOR,
+            ),
+          ),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(label),
+            Icon(
+              Icons.arrow_forward_ios,
+              color: GREY_COLOR,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/values/colors.dart
+++ lib/values/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const PRIMARY_COLOR = const Color(0xFF689ce8);
 const BACKGROUND_COLOR = const Color(0xFFF3F3F3);
 const WHITE_COLOR = const Color(0xFFFFFFFF);
+const GREY_COLOR = const Color(0xFFbabbba);
